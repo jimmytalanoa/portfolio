6fdfa74801acb7b1513cfbf1889e5d72e0e7d3b4
Loryn McNamara <jimmytalanoa@gmail.com>
2024-06-15 23:05:58 +1000

    added dark mode

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1 className="text-4x1 md:text-7x1 mb-1 md:mb-3 font-bold">James Talanoa</h1>
+      <h1 className="text-4x1 md:text-7x1 dark:text-white mb-1 md:mb-3 font-bold">James Talanoa</h1>
       <p className="text-base md:text-x1  mb-3 font-medium">Software Developer</p>
       <p className="text-sm max-w-xl mb-6 font-bold">I am a self taught software developer in the Logistics and Transport industry, building full stack applications to allow companies to communicate directly with the shipping ports.</p>
     </div>
--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 function Timeline({ year, title, duration, details}) {
   return (
-    <ol className="flex flex-col md:flex-row relative boder-l border-gray-500">
+    <ol className="flex flex-col md:flex-row relative border-l border-gray-200">
         <li className="mb-10 ml-4">
-            <div className="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -left-1.5 border border-white"/>
+            <div className="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -left-1.5 border border-white dark:border-stone-900 dark:bg-stone-700"/>
 
             <div className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
-                <span className="inline-block px-2 py-1 font-semibold bg-gray-500 text-white rounded-md">{year}</span>
-                <h3 className="text-lg font-semibold text-stone-900">{title}</h3>
+                <span className="inline-block px-2 py-1 font-semibold bg-gray-500 dark:bg-white text-white dark:text-stone-700 rounded-md">{year}</span>
+                <h3 className="text-lg font-semibold text-stone-900 dark:text-white">{title}</h3>
                 <div className="my-1 text-sm font-normal leading-none text-stone-400">{duration}</div>
                 
             </div>
-            <p className="my-2 text-base font-normal text-stone-500">{details}</p>
+            <p className="my-2 text-base font-normal text-stone-500 dark:text-stone-400">{details}</p>
         </li>
     </ol>
   );
