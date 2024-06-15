9846b600e39fed20057bc1a6744753a4fc7dde79
Loryn McNamara <jimmytalanoa@gmail.com>
2024-06-15 14:38:08 +1000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+import Contact from './components/Contact.jsx';
+import Footer from './components/Footer.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Timeline from './components/Timeline.jsx';
+import Intro from './components/Intro.jsx';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Intro />
+      <Timeline />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App; 
+ 
