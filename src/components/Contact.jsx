35e3f322e029d6433d2d3e6c08c7dbabcbd89d31
Loryn McNamara <jimmytalanoa@gmail.com>
2024-06-15 22:41:46 +1000

    added titles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 function Contact(){
     return (
         <div className='flex flex-col mb-10 mx-auto'>
             <div className="flex justify-center items-center">
                 <form action="https://getform.io/f/zbxdkgnb" method="POST" className="flex flex-col w-full md:w-7/12">
+                <Title>Contact</Title>
                 <input type="text" name="name" placeholder="Name" className="p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
                 <input type="email" name="email" placeholder="Email" className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
                 <textarea name="message" placeholder="Message" className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none" rows="10" />
