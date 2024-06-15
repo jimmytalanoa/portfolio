b939cb2d7685351b4609d6b79e8dd1da5a7b3cc0
Loryn McNamara <jimmytalanoa@gmail.com>
2024-06-15 22:31:14 +1000

    added contact details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 function Contact(){
     return (
-        <div>
-            Contact
+        <div className='flex flex-col mb-10 mx-auto'>
+            <div className="flex justify-center items-center">
+                <form action="https://getform.io/f/zbxdkgnb" method="POST" className="flex flex-col w-full md:w-7/12">
+                <input type="text" name="name" placeholder="Name" className="p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
+                <input type="email" name="email" placeholder="Email" className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
+                <textarea name="message" placeholder="Message" className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none" rows="10" />
+                <button type="submit" className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white">
+                    Work with me
+                </button>
+                </form>
+                </div>
         </div>
     );
 }
